a31e5ef83788fe36f4cd6e4a3123ab3b568c6411
ahmetcan.ersoz <ahmetcan.ersoz@ulakhaberlesme.com.tr>
2018-11-01 18:52:38 +0300

    float

--- src/main/java/Version.java
+++ src/main/java/Version.java
@@ -2,7 +2,8 @@ public class Version {
 
     public static void main(String[] args) {
 
-        System.out.println("Version 1.4.3");
+        //System.out.println("Version 1.4.3");
+        System.out.println("float");
 
     }
 }
